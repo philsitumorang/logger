6d9ae4d83c3f0da33e029d0589b1a91bfbded8ed
Phil Situmorang <philmsk@gmail.com>
2019-07-26 14:04:42 +0300

    add logger

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:12.4.0-alpine
+
+ENV PORT 4000
+ENV HTTP_AUTH_USER logger
+ENV HTTP_AUTH_PASSWORD rt4241gg
+
+RUN /bin/sh -c "apk add --no-cache bash nano"
+RUN mkdir -p /logger/src
+COPY package.json /logger
+COPY ./src /logger/src/
+WORKDIR /logger
+RUN npm i && npm i -g ts-node
+RUN npm install -g ts-node-dev typescript
+CMD npm run start-dev
---
+++ src/logger.ts
@@ -0,0 +1,75 @@
+import * as express from 'express';
+import { Server } from 'http';
+import * as SocketIO from 'socket.io';
+import * as basicAuth from 'basic-auth-connect';
+import * as moment from 'moment';
+import * as mkdirp from 'mkdirp';
+import * as cors from 'cors';
+import * as helmet from 'helmet';
+import * as bodyParser from 'body-parser';
+import { promises as fs } from 'fs';
+
+const app = express();
+const http = new Server(app);
+const io = SocketIO(http);
+
+const logPath = `${__dirname}/logs`;
+mkdirp(logPath, (err, made) => {});
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+
+app.get(
+  '/',
+  basicAuth(process.env.HTTP_AUTH_USER, process.env.HTTP_AUTH_PASSWORD),
+  (req, res) => {
+    res.sendFile(`${__dirname}/logger.html`);
+});
+
+app.get(
+  '/logger/:date',
+  basicAuth(process.env.HTTP_AUTH_USER, process.env.HTTP_AUTH_PASSWORD),
+  (req, res) => {
+    try {
+      res.sendFile(`${__dirname}/logs/${req.params.date}.log`);
+    } catch (err) {
+      res.send({
+        err: 'Log not found'
+      });
+    }
+});
+
+io.on('connection', (socket) => {
+  socket.on('save-log', (data) => {
+    socket.broadcast.emit('get-log', data);
+    fs.appendFile(`${__dirname}/logs/${moment().format('DD.MM.YYYY')}.log`, data + '\n');
+  });
+
+  app.post(
+    '/logger',
+    basicAuth(process.env.HTTP_AUTH_USER, process.env.HTTP_AUTH_PASSWORD),
+    async (req, res) => {
+      try {
+        await fs.appendFile(`${__dirname}/logs/${moment().format('DD.MM.YYYY')}.log`, req.body.dataLog + '\n');
+        socket.broadcast.emit('get-log', req.body.dataLog);
+        res.send({
+          saved: true
+        });
+      } catch (err) {
+        res.send({
+          err
+        })
+      }
+    });
+
+  console.log('[socket.io] user connected');
+  socket.on('disconnect', () => {
+    console.log('[socket.io] user disconnected');
+  });
+});
+
+http.listen(process.env.PORT, function () {
+  console.log(`listening on *:${process.env.PORT}`);
+});
